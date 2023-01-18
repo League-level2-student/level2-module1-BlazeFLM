6a8684878d20375d285bcb4f674bf2482810e6cd
student <student@ip-172-31-58-4.us-west-2.compute.internal>
2023-01-17 18:25:28 -0800

    6:25 PM 1/17/23

--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -16,21 +16,30 @@ public class VisualArrayLists implements ActionListener {
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
-		
+		list.add("Haha");
+		list.add("LOL");
+		list.add("aha");
+		list.add("jajajajaja");
+		list.add("kkkkkkkkkk");
 		// 2. Set one item in our list to be something different
-		
+		list.set(0, "HAHAHA");
 		// 3. Remove something from our list
-		
+		list.remove(3);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).equals("LOL")){
+				System.out.println("LOL is in the list");
+			}
+		}
 		// 6. Get a specific item from our list and print it out
-		
+		System.out.println(list.get(3));
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
 	
 	
